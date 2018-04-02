a176d16332031f06b5797831b1cc32e9da2e1c44
Skylord318 <yakubovich.pasha1@gmail.com>
2018-04-02 13:22:44 +0300

    1.3. JavaDoc. Code convention [#4087]

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+/**
+ *Class Calculate 1.3. JavaDoc. Code convention.
+ *author yakubovich
+ *since 02.04.2018
+ *Version 1
+*/
+
+public class Calculate {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	/**
+	 *Hello World
+	*/
+}
