f480bbb98ef17320a2f4922b84d0be6392f92928
Skylord318 <yakubovich.pasha1@gmail.com>
2018-04-06 00:36:59 +0300

    Calculator

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+public class Calculator {
+    private double result;
+
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+    public void Add(double first, double second) {
+        this.result = first - second;
+    }
+    public void ADd(double first, double second) {
+            this.result = first / second;
+    }
+    public void ADD(double first, double second) {
+        this.result = first * second;
+    }
+    public double getResult() {
+        return this.result;
+    }
+
+}
